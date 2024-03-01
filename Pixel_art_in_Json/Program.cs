3cfb940839c1fffc61373dacbc32c5e4accb7f07
CoCuCka-XD <70099291+CoCuCka-XD@users.noreply.github.com>
2024-03-01 20:16:18 +0700

    Done. maybe add multi-thread support for converting more than one image at one time.

--- Pixel_art_in_Json/Program.cs
+++ Pixel_art_in_Json/Program.cs
@@ -1,7 +1,207 @@
-﻿class TestClass
+﻿using Structures;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+class MainClass
 {
+    public static int NULL_cnt, COLOR_cnt = 0;
+    public static List<Structures.Color>? pallete;
+
+    public static bool Color_exist(Structures.Color Color)
+    {
+        if (pallete == null)
+        {
+            pallete = new List<Structures.Color>();
+            pallete.Add(Color);
+        }
+
+        foreach (Structures.Color clr in pallete)
+        {
+            if (clr.Equals(Color)) return true;
+        }
+
+        pallete.Add(Color);
+        return false;
+    }
+
+    public static int Serch_clr_id(Structures.Color color)
+    {
+        Color_exist(color);
+        for (int id = 0; id < pallete.Count; id++)
+        {
+            if (pallete[id].Equals(color)) return id;
+        }
+        return -1;
+    }
+
+    public static int Get_block_index_by_pos(float x, float y, Block[] blocks)
+    {
+        Block to_check = null;
+        foreach(Block block in blocks)
+        {
+            if (block.x == x && block.y == y) {
+                to_check = block;
+                break;
+            }
+        }
+        return blocks.ToList().IndexOf(to_check);
+
+
+    }
+
+    public static Block Get_block_by_pos(float x, float y, Block[] blocks)
+    {
+        foreach (Block block in blocks)
+        {
+            if (block.x == x && block.y == y) 
+            {
+                return block;
+            }
+        }
+        return null;
+    }
+
+    public static int Get_index_by_block(Block block, Block[] blocks)
+    {
+        return blocks.ToList().IndexOf(block);
+    }
+
+    public static bool Is_optimizeble(Block block, Block[] blocks_array, int size, Size img_size)
+    {
+        int x = (int)block.x;
+        int y = (int)block.y;
+
+        if (x + size > img_size.Width || y + size > img_size.Height) return false;
+        for (int X = x; X < x + size; X++)
+        {
+            for (int Y = y; Y < y + size; Y++)
+            {
+                Block to_check = Get_block_by_pos(X, Y, blocks_array);
+                if (to_check == null)
+                {
+                    return false;
+                }
+                if (to_check.color_id != block.color_id)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static Block[] Optimize(Block[] blocks, Size size)
+    {
+        // Optimization algorithm
+        List<Block> new_blocks = blocks.ToList();
+        List<Block> to_fill = new List<Block>();
+        bool flag = false;
+        
+
+
+        for (int pixel_size = 4; pixel_size > 1; pixel_size -= 2)
+        {
+            List<Block> blocks_to_check = new_blocks.ToList();
+
+            while (true)
+            {
+                flag = true;
+
+                //List<Block> local_BTC = blocks_to_check;
+                foreach (Block block in blocks_to_check.ToList())
+                {
+                    blocks_to_check.Remove(block);
+
+                    if (Is_optimizeble(block, new_blocks.ToArray(), pixel_size, size))
+                    {
+                        flag = false;
+                        float X = block.x;
+                        float Y = block.y;
+
+                        for (int x = (int)X; x < X + pixel_size; x++)
+                        {
+                            for (int y = (int)Y; y < Y + pixel_size; y++)
+                            {
+                                int to_remove = Get_block_index_by_pos(x, y, new_blocks.ToList().ToArray<Block>());
+                                if (to_remove != -1) new_blocks.RemoveAt(to_remove);
+                            }
+                        }
+                        bool condition = pixel_size == 4;
+                        float offset_multipling = condition ? 1.5f : 0.5f;
+
+                        to_fill.Add(new Block(
+                                condition ? (int)Block_ID.Full_Pixel : (int)Block_ID.Quadro_Pixel,
+                                block.x + offset_multipling,
+                                block.y + offset_multipling,
+                                block.color_id
+                        ));
+
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        foreach  (Block block in to_fill)
+        {
+            new_blocks.Add(block);
+        }
+        return new_blocks.ToArray();
+    }
+
+    public static JSON_Image Convert(Bitmap image)
+    {
+        image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+        Size size = image.Size;
+        Block[] blocks = new Block[size.Width * size.Height];
+        
+        for (int index = 0; index < size.Width * size.Height; index++)
+        {
+            int xPos = index % size.Width;
+            int yPos = (int)(index / size.Width);
+            Structures.Color curr_color = new Structures.Color(image.GetPixel(xPos, yPos));
+            blocks[index] = new Block(((int)Block_ID.Pixel), xPos, yPos, Serch_clr_id(curr_color));
+        }
+
+        List<Block> blocks_to_check = new List<Block>();
+        foreach (Block block in blocks)
+        {
+            if (pallete[block.color_id].a == 0) continue;
+            blocks_to_check.Add(block);
+        }
+
+        int[] arr_size = new int[2] { size.Width, size.Height };
+
+        Console.WriteLine($"Before optimization: {blocks.Length} blocks");
+
+        Block[] new_blocks = Optimize(blocks_to_check.ToArray<Block>(), size);
+
+        Console.WriteLine($"Optimization result: {new_blocks.Length} blocks");
+
+
+        return new JSON_Image(arr_size, pallete.ToArray(), new_blocks);
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine(args);
+        Stopwatch stopwatch = new Stopwatch();
+
+        Bitmap img = new Bitmap("C:\\Users\\DENIS\\Desktop\\kris_walk.png", false);
+        string fileName = "C:\\Users\\DENIS\\Desktop\\kris_walk.json";
+         
+        stopwatch.Start();
+        string jsonString = JsonSerializer.Serialize(Convert(img));
+        stopwatch.Stop();
+
+        Console.WriteLine($"Complete in: {stopwatch.ElapsedMilliseconds} milliseconds");
+        File.WriteAllText(fileName, jsonString);
     }
 }
---
+++ Pixel_art_in_Json/Structures.cs
@@ -0,0 +1,64 @@
+﻿namespace Structures
+{
+    internal enum Block_ID
+    {
+        Pixel = 917,
+        Quadro_Pixel = 916,
+        Full_Pixel = 955
+    }
+
+    internal class Block
+    {
+        public int obj_id { get; set; }
+        public float x { get; set; }
+        public float y { get; set; }
+        public int color_id { get; set; }
+        public Block(int OBJ_ID, float X, float Y, int COLOR_ID)
+        {
+            obj_id = OBJ_ID;
+            x = X;
+            y = Y;
+            color_id = COLOR_ID;
+        }
+
+        public bool Equals(Block other) => this.obj_id == other.obj_id && this.x == other.x && this.y == other.y && this.color_id == other.color_id;
+    }
+
+    internal class Color
+    {
+        public int r { get; set; }
+        public int g { get; set; }
+        public int b { get; set; }
+        public int a { get; set; }
+        public Color(int R, int G, int B, int A)
+        {
+            r = R; g = G; b = B; a = A;
+        }
+
+        public Color(System.Drawing.Color color)
+        {
+            r = color.R; g = color.G; b = color.B; a = color.A;
+        }
+
+        public int[] ToArray() => new int[4] { this.r, this.g, this.b, this.a };
+
+        public bool Equals(Color other) => this.r == other.r && this.g == other.g && this.b == other.b && this.a == other.a;
+
+    }
+
+    internal class JSON_Image
+    {
+        public int[] size { get; set; } = new int[2];
+        public Array[] Blocks { get; set; }
+        public Array[] Colors_set { get; set; }
+        public JSON_Image(int[] size, Color[] colors_set, Block[] blocks)
+        {
+            this.size = size;
+            Blocks = new Array[blocks.Length];
+            Colors_set = new Array[colors_set.Length];
+
+            for (int i = 0; i < blocks.Length; i++) this.Blocks[i] = new float[4] { blocks[i].obj_id, blocks[i].x, blocks[i].y, blocks[i].color_id };
+            for (int i = 0; i < colors_set.Length; i++) this.Colors_set[i] = new float[4] { colors_set[i].r, colors_set[i].g, colors_set[i].b, colors_set[i].a };
+        }
+    }
+}
